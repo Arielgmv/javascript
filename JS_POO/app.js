6949970940f46f28dc7e267654bd7979406a31a6
Arielgmv <ariel.munoz.villegas@gmail.com>
2018-10-17 18:43:14 -0400

    Avances_(17-10-18)

--- JS_POO/app.js
+++ JS_POO/app.js
@@ -11,9 +11,21 @@ class Pelicula {
         this.nombre= nombre;
         this.id=id;
     }
+    reproducir(){
+        return `Reproduciendo una película ${this.nombre}`;
+    }
 }
+
+class Serie extends Pelicula{
+    reproducirCapitulo(){
+        return `Reproduciendo capítulo...${this.nombre}`;
+    }
+}
+
 const peliculaUno=new Pelicula('Harry',1);
 const peliculaDos=new Pelicula('Araña',2);
+const serieUno=new Serie('Dexter', 3);
 
-console.log(peliculaUno);
-console.log(peliculaDos);
+console.log(peliculaUno.reproducir());
+console.log(peliculaDos.reproducir());
+console.log(serieUno.reproducirCapitulo());
