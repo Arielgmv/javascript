1ee92409c1c4541559a846b5648dac1918d18fa8
Arielgmv <ariel.munoz.villegas@gmail.com>
2018-10-31 20:23:33 -0400

    Avances_(31-10-18)

--- Aplicacion_Productos/App.js
+++ Aplicacion_Productos/App.js
@@ -26,11 +26,23 @@ class UI{
         document.getElementById('product-form').reset();
     }
     
-    deleteProduct(){
-
+    deleteProduct(element){
+        if(element.name === 'delete'){
+            element.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Product deleted successfully', 'info');
+        }
     }
-    showMessage(){
-
+    showMessage(message, cssClass){
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-4`;
+        div.appendChild(document.createTextNode(message));
+        //Showing in DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        container.insertBefore(div, app);
+        setTimeout(function (){
+            document.querySelector('.alert').remove();
+        }, 2000)
     }
 }
 //DOM Events
@@ -41,12 +53,17 @@ const year=document.getElementById('year').value;
 
 const product=new Product(name, price, year);
 const ui=new UI();
+if(name === '' || price === '' || year === ''){
+    return ui.showMessage('Please Complete fields', 'danger');
+}
 ui.addProduct(product);
 ui.resetForm();
+ui.showMessage('Product added Successfully', 'success');
 
 e.preventDefault();
 });
 
 document.getElementById('product-list').addEventListener('click', function(e){
-    console.log(e.target)
+    const ui=new UI();
+    ui.deleteProduct(e.target); 
 });
