ecc01c95c509c92859e28cae2c6f141246c3cd81
Arielgmv <ariel.munoz.villegas@gmail.com>
2018-11-03 13:06:30 -0400

    Avances_(3-11-18)

---
+++ JS_Orientado_Objetos/javascript.js
@@ -0,0 +1,26 @@
+//Crear una clase
+class miClase{
+    constructor(primero, segundo){
+        this.uno=primero;
+        this.dos=segundo;
+        this.tres="Hola a todos";
+    };
+    miMetodoUno(){
+        return this.uno;
+    }
+    miMetodoDos(parametroUno, parametroDos){
+        document.write(
+            "El parametro UNO es: "+parametroUno+
+            "<br>El parametro DOS es: "+parametroDos
+        );
+    }
+};
+
+//Crear un objeto - instanciar
+var objetoUno = new miClase("Ariel", 777777);
+
+//Recuperar datos
+document.write(objetoUno.uno);
+document.write("<br>"+objetoUno.dos+"<br>");
+objetoUno.miMetodoDos("Ana", 3445);
+    
